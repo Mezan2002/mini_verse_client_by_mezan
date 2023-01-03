cae07535abf1c967ea3017f208aebce43dd0306a
Mezan2002 <mezanurr009@gmail.com>
2023-01-03 15:53:43 +0600

    Friend requests field created

--- src/Pages/MainHome/RightSideMenu/RightSideMenu.js
+++ src/Pages/MainHome/RightSideMenu/RightSideMenu.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { FaSearch } from "react-icons/fa";
 
 const RightSideMenu = () => {
   return (
     <div className="px-5">
       <div>
-        <div className="card bg-base-100 shadow-xl">
+        {/* messages card start */}
+        <div className="card shadow-xl">
           <div className="card-body">
+            {/* right side topbar start */}
             <div className="flex items-center justify-between">
               <h3 className="font-medium text-xl">Messages</h3>
               <img
@@ -15,8 +16,9 @@ const RightSideMenu = () => {
                 className="w-7"
               />
             </div>
+            {/* right side topbar end */}
             <div>
-              {/* right side topbar start */}
+              {/* message search field start */}
               <div className="relative flex items-center mb-5">
                 <input
                   type="text"
@@ -30,7 +32,7 @@ const RightSideMenu = () => {
                   alt=""
                 />
               </div>
-              {/* right side topbar end */}
+              {/* message search field end */}
               {/* tabs start */}
               <div>
                 <div className="tabs w-full">
@@ -43,7 +45,46 @@ const RightSideMenu = () => {
               {/* messages start */}
               <div className="mt-5">
                 {/* single message start */}
-                <div className="flex items-center">
+                <div className="flex items-center mb-6">
+                  <div className="avatar">
+                    <div className="w-14 rounded-full">
+                      <img src="https://placeimg.com/192/192/people" alt="" />
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <h5 className="font-semibold">Mehedi Hasan</h5>
+                    <p className="text-gray-500">Pick the call.</p>
+                  </div>
+                </div>
+                {/* single message end */}
+                {/* single message start */}
+                <div className="flex items-center mb-6">
+                  <div className="avatar">
+                    <div className="w-14 rounded-full">
+                      <img src="https://placeimg.com/192/192/people" alt="" />
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <h5 className="font-semibold">Muhit Khan</h5>
+                    <p className="text-gray-500">Hey, Please Join Us.</p>
+                  </div>
+                </div>
+                {/* single message end */}
+                {/* single message start */}
+                <div className="flex items-center mb-6">
+                  <div className="avatar">
+                    <div className="w-14 rounded-full">
+                      <img src="https://placeimg.com/192/192/people" alt="" />
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <h5 className="font-semibold">Mukit Khan</h5>
+                    <p className="text-gray-500">Go to our home.</p>
+                  </div>
+                </div>
+                {/* single message end */}
+                {/* single message start */}
+                <div className="flex items-center mb-6">
                   <div className="avatar">
                     <div className="w-14 rounded-full">
                       <img src="https://placeimg.com/192/192/people" alt="" />
@@ -55,8 +96,32 @@ const RightSideMenu = () => {
                   </div>
                 </div>
                 {/* single message end */}
+                {/* single message start */}
+                <div className="flex items-center mb-6">
+                  <div className="avatar">
+                    <div className="w-14 rounded-full">
+                      <img src="https://placeimg.com/192/192/people" alt="" />
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <h5 className="font-semibold">Robiul Islam</h5>
+                    <p className="text-gray-500">Come on the party</p>
+                  </div>
+                </div>
+                {/* single message end */}
               </div>
               {/* messages end */}
+            </div>
+          </div>
+        </div>
+        {/* messages card end */}
+        <div className="mt-2">
+          <div className="card shadow-xl">
+            <div className="card-body">
+              <div className="flex items-center justify-between">
+                <h3 className="font-medium text-xl">Friend Reqestes</h3>
+                <h6 className="mt-2">See All</h6>
+              </div>
             </div>
           </div>
         </div>
